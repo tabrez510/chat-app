530d724ac6aada4e33b4ff16416271b844a4cdee
tabrez510 <alamtabrez510@gmail.com>
2024-02-22 21:27:57 +0530

    CORS-added

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://127.0.0.1:5501"
+}));
 
 app.use(bodyParser.json());
 
